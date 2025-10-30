e62bec9d4b18dcd9d851ff1e4ce277c2b67d789e
Alexm-68 <almecht@gmail.com>
2025-10-31 00:53:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ icon = pygame.image.load('img/icon.jpg')
 pygame.display.set_icon(icon)
 
 target_image = pygame.image.load('img/target.png')
-target_width = 80
-target_height = 80
+target_width, target_height = target_image.get_size()
 target_x = random.randint (0, SCREEN_WIDTH - target_width)
 target_y = random.randint (0, SCREEN_HEIGHT - target_height)
 
@@ -26,11 +25,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-            target_x = random.randint (0, SCREEN_WIDTH - target_width)
-            target_y = random.randint (0, SCREEN_HEIGHT - target_height)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint (0, SCREEN_WIDTH - target_width)
+                target_y = random.randint (0, SCREEN_HEIGHT - target_height)
 
 
     screen.blit(target_image, (target_x, target_y))
